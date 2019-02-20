42fcbab5ada5109ae21133258b86f99c2a2fff5c
Anton Schulte <mail@anton-schulte.de>
2019-02-20 11:46:21 +0100

    finalley fixed type

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,3 @@
-declare module 'xrandr-evt' {
-  import { EventEmitter }                           from 'events';
 
-  function createEmiiter(): EventEmitter;
-  let numberOfGreetings: number;
-}
+import { EventEmitter }                           from 'events';
+export function getEmitter(): EventEmitter;
